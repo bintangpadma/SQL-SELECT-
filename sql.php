0cbc7a2267b6013b658947606206e4f1162fbda3
bintangpadma <97139210+bintangpadma@users.noreply.github.com>
2023-11-20 09:22:30 +0700

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020003_Bintang Padma";
+
+    $conn    = mysqli_connect($server,$user,$password,$database);
+
+    ?>
